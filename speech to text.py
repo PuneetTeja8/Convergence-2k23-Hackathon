61efa213f25f9de297a7bc4e29249a240b87b0d1
Puneet Teja <119788700+PuneetTeja8@users.noreply.github.com>
2023-01-24 12:17:13 +0530

    Create speech to text.py

---
+++ speech to text.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+r = sr.Recognizer()
+
+mic = sr.Microphone()
+
+print('start')
+with mic as source:
+    audio = r.listen(source)
+print('end')
+print(r.recognize_google(audio))
