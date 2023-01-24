85eb5b270dd9f52ef959946965cced1f77b74c9d
Zeustheprogrammer <74101459+Zeustheprogrammer@users.noreply.github.com>
2023-01-24 22:05:07 +0530

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,22 +1,81 @@
+import os
+import openai
 import gradio as gr
+from playsound import playsound
+from gtts import gTTS
 import speech_recognition as sr
 
-def my_input_func(text_input, voice_input):
-    return f"Text Input: {text_input}, Voice Input: {voice_input}"
 
-def voice_input():
-    r = sr.Recognizer()
+openai.api_key = "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"
+
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+
+prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
+
+def openai_create(prompt):
+
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )
+
+    return response.choices[0].text
+
+
+
+def chatgpt_clone(input, history):
+    history = history or []
+    s = list(sum(history, ()))
+    s.append(input)
+    inp = ' '.join(s)
+    output = openai_create(inp)
+    history.append((input, output))
+    mytext = output
+    language = 'en'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("welcome.mp3")
+    playsound('C:\\Users\\abboj\\Downloads\\Hackathon-main\\Hackathon-main\\welcome.mp3')
+    return history, history
+
+def voice():
+    recognizer = sr.Recognizer()
+
     with sr.Microphone() as source:
-        audio = r.listen(source)
+        print("Adjusting noise ")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+        print("Recording for 4 seconds")
+        recorded_audio = recognizer.listen(source, timeout=4)
+        print("Done recording")
+
     try:
-        return r.recognize_google(audio)
-    except sr.UnknownValueError:
-        return "Google Speech Recognition could not understand audio"
-    except sr.RequestError as e:
-        return f"Could not request results from Google Speech Recognition service; {e}"
+        print("Recognizing the text")
+        text = recognizer.recognize_google(
+                recorded_audio, 
+                language="en-US"
+            )
+    except Exception as ex:
+        print(ex)
+    return text
 
-inputs = [gr.inputs.Textbox(label="Text Input"), 
-          gr.inputs.Func(label="Voice Input", func=voice_input)]
-output = gr.outputs.Textbox()
+block = gr.Blocks()
 
-gr.Interface(my_input_func, inputs, output, layout="column").launch()
+
+with block:
+    gr.Markdown("""<h1><center>Alpha Q</center></h1>
+    """)
+    chatbot = gr.Chatbot()
+    message = gr.Textbox(placeholder=prompt)
+    state = gr.State()
+    voicef = gr.Button("VOICE")
+    voicef.click(voice, outputs=message)
+    submit = gr.Button("SEND")
+    submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
+
+block.launch(debug = True)
